75779bf7c931d261ff4aead36bc14bd1b3ec9f7a
shalocklove <609145339@qq.com>
2017-07-21 13:35:13 +0800

    Add files via upload

--- java爬虫/Intact/Demo.java
+++ java爬虫/Intact/Demo.java
@@ -1,10 +1,11 @@
 package Intact;
 
+import java.sql.DriverManager;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -12,16 +13,13 @@ import net.sf.json.JSONObject;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Bean b = new Bean();
 		Reptile reptile = new Reptile();
 		JSONObject json = reptile.getJson("http://www.wipo.int/madrid/gazette/query.jsp", "action=query&lang=en&page=null&year=2017&issue=26&chapter=1&country=BX&origin=on&interested=on&designated=on&irn=&holder=&brand=&type=ENN&start=0&rows=10&context=browse");
 		JSONArray docs = (JSONArray) json.get("docs");
 		System.out.println(docs);
-		JSONObject other = docs.getJSONObject(0);
 		JSONObject j;
 		System.out.println(docs.size());
-		List<Bean> l = new ArrayList();
+		List<Bean> l = new ArrayList<Bean>();
 		
 		for (int i = 0; i < docs.size(); i++) {
 			Bean e = new Bean();
@@ -35,10 +33,22 @@ public class Demo {
 			e.setTransacition("Registration");
 			l.add(e);
 		}
-		System.out.println(l.size());
-		for (Bean bean : l) {
-			System.out.println(bean);
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "sherlock");
+			Statement stmt = (Statement) con.createStatement();
+			for (Bean bean : l) {
+				stmt.executeUpdate("insert into demo1 (Mark, IRN, Origin, Holder, Transacition, Gazette)"+
+				"values('"+bean.getMark()+"','"+bean.getIRN()+"','"+bean.getOrigin()+"','"+bean.getHolder()+"','"+bean.getTransacition()+"','"+bean.getGazette()+"')");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		finally{
+			
+		}
+		System.out.println(l.size());
 	}
 
 }
